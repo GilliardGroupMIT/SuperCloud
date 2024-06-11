9016bd4ef6fceda9ffd4f13092dc48b6d051d764
amolino-1 <molino.andrew@gmail.com>
2024-06-11 11:38:35 -0400

    fixed path to slurm script

--- opt/utilities/submit.sh
+++ opt/utilities/submit.sh
@@ -6,11 +6,11 @@ if [ ! -f slurm_master ]; then
     echo "WARNING: SLURM master file does not exist - Using 24Hr SLURM template"
     for SLURMJOB in $(seq 1 $files); do
         if [[ "$SLURMJOB" -lt 10 ]]; then
-            cp -v $HOME/templates/slurm_orca504.sh slurm_0$SLURMJOB
+            cp -v $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh slurm_0$SLURMJOB
             sed -i "s/*.inp/0${SLURMJOB}*.inp/" slurm_0$SLURMJOB
             sbatch slurm_0$SLURMJOB
         else
-            cp -v $HOME/templates/slurm_orca504.sh slurm_$SLURMJOB
+            cp -v $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh slurm_$SLURMJOB
             sed -i "s/*.inp/${SLURMJOB}*.inp/" slurm_$SLURMJOB
             sbatch slurm_$SLURMJOB
         fi
