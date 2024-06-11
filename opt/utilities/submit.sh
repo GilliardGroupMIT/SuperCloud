4f62a105dcce19b5c3b1ad31d3858af76482e450
amolino-1 <molino.andrew@gmail.com>
2024-06-11 11:40:39 -0400

    cleaned up submit, only works for orca

--- opt/utilities/submit.sh
+++ opt/utilities/submit.sh
@@ -4,27 +4,27 @@ files=$(ls -1 | grep -c inp)
 
 if [ ! -f slurm_master ]; then
     echo "WARNING: SLURM master file does not exist - Using 24Hr SLURM template"
-    for SLURMJOB in $(seq 1 $files); do
+    for SLURMJOB in $(seq 1 "$files"); do
         if [[ "$SLURMJOB" -lt 10 ]]; then
-            cp -v $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh slurm_0$SLURMJOB
-            sed -i "s/*.inp/0${SLURMJOB}*.inp/" slurm_0$SLURMJOB
-            sbatch slurm_0$SLURMJOB
+            cp -v "$HOME"/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh slurm_0"$SLURMJOB"
+            sed -i "s/*.inp/0${SLURMJOB}*.inp/" slurm_0"$SLURMJOB"
+            sbatch slurm_0"$SLURMJOB"
         else
-            cp -v $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh slurm_$SLURMJOB
-            sed -i "s/*.inp/${SLURMJOB}*.inp/" slurm_$SLURMJOB
-            sbatch slurm_$SLURMJOB
+            cp -v "$HOME"/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh slurm_"$SLURMJOB"
+            sed -i "s/*.inp/${SLURMJOB}*.inp/" slurm_"$SLURMJOB"
+            sbatch slurm_"$SLURMJOB"
         fi
     done
 else
-    for SLURMJOB in $(seq 1 $files); do
+    for SLURMJOB in $(seq 1 "$files"); do
         if [[ "$SLURMJOB" -lt 10 ]]; then
-            cp -v slurm_master slurm_0$SLURMJOB
-            sed -i "s/*.inp/0${SLURMJOB}*.inp/" slurm_0$SLURMJOB
-            sbatch slurm_0$SLURMJOB
+            cp -v slurm_master slurm_0"$SLURMJOB"
+            sed -i "s/*.inp/0${SLURMJOB}*.inp/" slurm_0"$SLURMJOB"
+            sbatch slurm_0"$SLURMJOB"
         else
-            cp -v slurm_master slurm_$SLURMJOB
-            sed -i "s/*.inp/${SLURMJOB}*.inp/" slurm_$SLURMJOB
-            sbatch slurm_$SLURMJOB
+            cp -v slurm_master slurm_"$SLURMJOB"
+            sed -i "s/*.inp/${SLURMJOB}*.inp/" slurm_"$SLURMJOB"
+            sbatch slurm_"$SLURMJOB"
         fi
     done
 fi
