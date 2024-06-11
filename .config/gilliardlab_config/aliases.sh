4c431ebd86d1ebc1df146ff059a204cd7a7a0289
amolino-1 <molino.andrew@gmail.com>
2024-06-11 15:22:54 -0400

    added submit alias

--- .config/gilliardlab_config/aliases.sh
+++ .config/gilliardlab_config/aliases.sh
@@ -1,6 +1,7 @@
 # Group Aliases
 alias stream='less -S +F'
 alias jobstat='$HOME/gilliardlab_shared/opt/utilities/jobstat.sh'
+alias submit='$HOME/gilliardlab_shared/opt/utilities/slurm_scripts/submit.sh'
 
 ### ORCA 5.0.4
 alias copyorca='cp $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh'
