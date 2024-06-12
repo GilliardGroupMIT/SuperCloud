8a1d76e23f031acc434e3acc2cba9825e9ab316d
amolino-1 <molino.andrew@gmail.com>
2024-06-12 11:00:28 -0400

    changed path to absolute

--- config/gilliardlab_config/aliases.sh
+++ config/gilliardlab_config/aliases.sh
@@ -1,10 +1,23 @@
+export GILLIARDLAB=/home/gridsan/groups/gilliardlab
+
 # Group Aliases
 alias stream='less -S +F'
-alias jobstat='$HOME/gilliardlab_shared/opt/utilities/jobstat.sh'
-alias submit='$HOME/gilliardlab_shared/opt/utilities/slurm_scripts/submit.sh'
+alias jobstat='$GILLIARDLAB/opt/utilities/jobstat.sh'
+alias submit='$GILLIARDLAB/opt/utilities/slurm_scripts/submit.sh'
+
+rename() {
+    local from=$1
+    local to=$2
+    shift 2
+    local files="$@"
+
+    for file in $files; do
+        $GILLIARDLAB/opt/utilities/rename.pl "s/$from/$to/" "$file"
+    done
+}
 
 ### ORCA 5.0.4
-alias copyorca='cp $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_orca504.sh'
+alias copyorca='cp $GILLIARDLAB/opt/utilities/slurm_scripts/slurm_orca504.sh'
 
 ### Gaussian 16
-alias copyg16='cp $HOME/gilliardlab_shared/opt/utilities/slurm_scripts/slurm_gaussian16.sh'
+alias copyg16='cp $GILLIARDLAB/opt/utilities/slurm_scripts/slurm_gaussian16.sh'
--- config/gilliardlab_config/env_vars.sh
+++ config/gilliardlab_config/env_vars.sh
@@ -1,12 +1,14 @@
-export GILLIARDGRP=$HOME/gilliardlab_shared
+export GILLIARDLAB=/home/gridsan/groups/gilliardlab
+
+export PATH=$PATH:GILLIARDLAB/.local/bin
 
 ### ORCA 5.0.4
-export ORCA_PATH=$HOME/gilliardlab_shared/opt/chemistry/Orca_504
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/gilliardlab_shared/opt/chemistry/Orca_504
+export ORCA_PATH=$GILLIARDLAB/gilliardlab_shared/opt/chemistry/Orca_504
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GILLIARDLAB/opt/chemistry/Orca_504
 export PATH=$PATH:$ORCA_PATH
 
 ### Gaussian16
-export g16root=$GILLIARDGRP/opt/chemistry/Gaussian16_A03/
+export g16root=$GILLIARDLAB/opt/chemistry/Gaussian16_A03/
 export GAUSS_EXEDIR=$g16root/g16
 export GAUSS_SCRDIR=$g16root/scr
 export PATH=$PATH:$GAUSS_EXEDIR
