ba23aab699e3f7fe2103f6df0c44727cd9bb3d0f
amolino-1 <molino.andrew@gmail.com>
2024-07-10 09:57:53 -0400

    fixed path to orca

--- config/gilliardlab_config/env_vars.sh
+++ config/gilliardlab_config/env_vars.sh
@@ -5,7 +5,7 @@ GILLIARDLAB=/home/gridsan/groups/gilliardlab
 export PATH=$PATH:GILLIARDLAB/.local/bin
 
 ### ORCA 5.0.4
-export ORCA_PATH=$GILLIARDLAB/gilliardlab_shared/opt/chemistry/Orca_504
+export ORCA_PATH=$GILLIARDLAB/opt/chemistry/Orca_504
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GILLIARDLAB/opt/chemistry/Orca_504
 export PATH=$PATH:$ORCA_PATH
 
