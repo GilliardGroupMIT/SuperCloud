c4a113a2c3905185f8465cbebbf5ad44965c2321
amolino-1 <molino.andrew@gmail.com>
2024-09-07 23:00:52 -0400

    removed use_scratch

--- opt/utilities/slurm_scripts/slurm_orca600.sh
+++ opt/utilities/slurm_scripts/slurm_orca600.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=12:00:00
 #SBATCH --mem=66GB
 #SBATCH --output=/dev/null
-use_scratch=false
 
 input_files="*.inp"
 
@@ -17,6 +16,11 @@ GILLIARDLAB=/home/gridsan/groups/gilliardlab
 ORCA_PATH=$GILLIARDLAB/opt/chemistry/Orca_600_avx2
 NBOEXE=$GILLIARDLAB/opt/chemistry/NBO6/bin/nbo6.i4.exe
 
+if [ -z "$GILLIARDLAB" ]; then
+    echo "Error: GILLIARDLAB environment variable is not set."
+    exit 1
+fi
+
 JOBDIR="$PWD"
 export JOBDIR
 export PATH=$JOBDIR:$PATH
@@ -25,58 +29,22 @@ for file in $input_files; do
     job_name=${file//.inp/}
     input_file=${job_name}.inp
     output_file=${job_name}.out
-    if [ "$use_scratch" = true ]; then
-        export SCRATCHDIR=$HOME/scratch
-        export PATH=$SCRATCHDIR:$PATH
-        export ORCATEMPDIR=$SCRATCHDIR
-        if [ ! -d "$ORCATEMPDIR/${job_name}"_tmp ]; then
-            mkdir -p "$ORCATEMPDIR/${job_name}"_tmp
-            export TEMPDIR=$ORCATEMPDIR/${job_name}_tmp
+    export ORCATEMPDIR=$JOBDIR
+    mkdir -p "$ORCATEMPDIR/${job_name}"_tmp
+    export TEMPDIR=$ORCATEMPDIR/${job_name}_tmp
+    cp "$input_file" "$TEMPDIR"
+    dependencies=$(find "$JOBDIR" -type d -iname "dependencies" -print -quit)
+    if [ -n "$dependencies" ]; then
+        dependency_errors=$(find "$dependencies" -type f -not -name "${job_name}_D?????.res.*")
+        if [ -n "$dependency_errors" ]; then
+            echo "Error: Input filename '$input_file' does not match dependency file name(s):"
+            exit 1
         else
-            num_dirs=$(find "$ORCATEMPDIR" -name "*${job_name}_tmp*" | wc -l)
-            if [ "$num_dirs" -lt 10 ]; then
-                mkdir -p "$ORCATEMPDIR/${job_name}_tmp_0$num_dirs"
-                export TEMPDIR=$ORCATEMPDIR/${job_name}_tmp_0${num_dirs}
-            else
-                mkdir -p "$ORCATEMPDIR/${job_name}_tmp_$num_dirs"
-                export TEMPDIR=$ORCATEMPDIR/${job_name}_tmp_${num_dirs}
-            fi
+            find "$dependencies" -type f -name "${job_name}_D?????.res.*" -exec cp {} "$TEMPDIR" \;
         fi
-        cp "$input_file" "$TEMPDIR"
-        dependencies=$(find "$JOBDIR" -type d -iname "dependencies" -print -quit)
-        if [ -n "$dependencies" ]; then
-            dependency_errors=$(find "$dependencies" -type f -not -name "${job_name}_D?????.res.*")
-            if [ -n "$dependency_errors" ]; then
-                echo "Error: Input filename '$input_file' does not match dependency file name(s):"
-                exit 1
-            else
-                find "$dependencies" -type f -name "${job_name}_D?????.res.*" -exec cp {} "$TEMPDIR" \;
-            fi
-        fi
-        cd "$TEMPDIR" || exit
-        "$ORCA_PATH"/orca "$input_file" >& "$output_file" 2>&1
-        cp "$TEMPDIR/$output_file" "$JOBDIR"/
-        cp -r "$TEMPDIR"/ "$JOBDIR"/
-        rm -r "${TEMPDIR:?}/"
-        cd "$JOBDIR" || exit
-    else
-        export ORCATEMPDIR=$JOBDIR
-        mkdir -p "$ORCATEMPDIR/${job_name}"_tmp
-        export TEMPDIR=$ORCATEMPDIR/${job_name}_tmp
-        cp "$input_file" "$TEMPDIR"
-        dependencies=$(find "$JOBDIR" -type d -iname "dependencies" -print -quit)
-        if [ -n "$dependencies" ]; then
-            dependency_errors=$(find "$dependencies" -type f -not -name "${job_name}_D?????.res.*")
-            if [ -n "$dependency_errors" ]; then
-                echo "Error: Input filename '$input_file' does not match dependency file name(s):"
-                exit 1
-            else
-                find "$dependencies" -type f -name "${job_name}_D?????.res.*" -exec cp {} "$TEMPDIR" \;
-            fi
-        fi
-        cd "$TEMPDIR" || exit
-        "$ORCA_PATH"/orca "$input_file" >& "$output_file" 2>&1
-        cp "$TEMPDIR/$output_file" "$JOBDIR"/
-        cd "$JOBDIR" || exit
     fi
+    cd "$TEMPDIR" || exit
+    "$ORCA_PATH"/orca "$input_file" >& "$output_file" 2>&1
+    cp "$TEMPDIR/$output_file" "$JOBDIR"/
+    cd "$JOBDIR" || exit
 done
