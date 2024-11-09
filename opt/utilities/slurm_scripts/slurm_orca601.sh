d0e0066c67d8a4838c84ad34faf88ffddc552163
amolino-1 <molino.andrew@gmail.com>
2024-11-08 21:26:57 -0500

    changed NBO from i4 to i8

--- opt/utilities/slurm_scripts/slurm_orca601.sh
+++ opt/utilities/slurm_scripts/slurm_orca601.sh
@@ -15,7 +15,7 @@ module load mpi/openmpi-4.1.5
 GILLIARDLAB=/home/gridsan/groups/gilliardlab
 ORCA_PATH=$GILLIARDLAB/opt/chemistry/Orca_601_avx2
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GILLIARDLAB/opt/chemistry/Orca_601_avx2
-NBOEXE=$GILLIARDLAB/opt/chemistry/NBO6/NBO6.i4/bin/nbo6.i4.exe
+NBOEXE=$GILLIARDLAB/opt/chemistry/NBO6/NBO6.i8/bin/nbo6.i8.exe
 
 if [ -z "$GILLIARDLAB" ]; then
     echo "Error: GILLIARDLAB environment variable is not set."
